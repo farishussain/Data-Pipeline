bf97a4e5aa71b79cf0b921764a5da5d02f1ac004
farishussain <farishussain021@gmail.com>
2024-10-26 18:32:15 +0000

    Docker Files Changes

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -1,4 +1,4 @@
-# ingest_data.py
+# scripts/ingest_data.py
 import requests
 import pandas as pd
 from datetime import datetime
@@ -6,14 +6,16 @@ from pyspark.sql import SparkSession
 from config import API_ENDPOINT, START_DATE, END_DATE, FREQUENCY
 
 # Initialize Spark
-spark = SparkSession.builder.appName("EnergyChartsIngestion").getOrCreate()
+spark = SparkSession.builder \
+    .appName("EnergyChartsIngestion") \
+    .config("spark.driver.extraJavaOptions", "--add-opens java.base/java.nio=ALL-UNNAMED") \
+    .getOrCreate()
 
 def fetch_data(api_url):
     response = requests.get(api_url)
     response.raise_for_status()
     return pd.DataFrame(response.json())
 
-# Example function for daily ingestion
 def daily_ingest(start_date=START_DATE, end_date=END_DATE):
     data = fetch_data(f"{API_ENDPOINT}/data?start_date={start_date}&end_date={end_date}")
     spark_df = spark.createDataFrame(data)
